d7c899a181b2fbf1106dfc7fe2ddaa03d8882f08
Artur Domurad <domurad.artur@gmail.com>
2021-11-11 09:23:13 +0100

    ✨ Simple navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
+import type { GetStaticPropsContext, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { getPostPages } from '../lib/api'
 import styles from '../styles/Home.module.css'
+import { IPost } from '../types/generated/contentful'
+import { format } from 'date-fns'
 
-const Home: NextPage = () => {
+const Home: NextPage<{ pages: IPost[] }> = ({ pages }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,33 +26,15 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          {pages.map(page => (
+            <a href={`/posts/${page.fields.name}`} className={styles.card} key={page.sys.id}>
+              <h2>{page.fields.name} &rarr;</h2>
+              <p>{page.fields.age}</p>
+              <p>{"Created at: " + format(new Date(page.sys.createdAt), 'd.LL.y HH:mm:SS')}</p>
           </a>
+          ))}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -70,3 +55,10 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const pages = await getPostPages();
+  return {
+    props: { pages }, // will be passed to the page component as props
+  }
+}
